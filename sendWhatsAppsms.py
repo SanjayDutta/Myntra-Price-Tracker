1d3d011aadb3543f4a5d525c4dd96a2c282722ae
SanjayDutta <sanjay.02.aug.1998@gmail.com>
2020-04-04 01:59:17 +0530

    Removed previous Twilio Information

--- sendWhatsAppsms.py
+++ sendWhatsAppsms.py
@@ -3,8 +3,8 @@ from twilio.rest import Client
 
 class sendWhatsAppsms:
     def __init__(self,message,tpi,tsec,tphone,phone):
-        account_sid = tpi #AC86c526e3d3b6c8b9885d4d818cbf12e9
-        auth_token = tsec #c2a18c1c07e2e6a170b4b22f825f0f31 #tphone:+14155238886
+        account_sid = tpi #
+        auth_token = tsec # #tphone:+14155238886
         client = Client(account_sid, auth_token)
 
         from_whatsapp_number = 'whatsapp:'+tphone
